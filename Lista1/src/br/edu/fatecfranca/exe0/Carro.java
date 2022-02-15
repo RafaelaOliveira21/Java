741673a3a09247b9eae5556b1bc14ef179971f0c
Rafaela Oliveira <rafaela.oliveira0202@hotmail.com>
2022-02-15 10:04:18 -0300

    Primeira aula de Java

---
+++ Lista1/src/br/edu/fatecfranca/exe0/Carro.java
@@ -0,0 +1,46 @@
+package br.edu.fatecfranca.exe0;
+
+public class Carro {
+	
+	// declaração das variáveis
+	// Java é fortemente tipado
+	
+	String modelo;
+	int ano;
+	float velocidade;
+	boolean motor; // motor true é ligado e false é desligado
+	
+	// criação dos métodos
+	void ligar () {
+		motor = true;
+	}
+	void desligar () {
+		motor = false;
+		velocidade = 0;
+	}
+	void acelerar(float x) { //x representa o quanto será acelerado
+		if (motor) {
+			velocidade += x;
+		}
+	}
+	void frear (float x) {
+		if (motor) {
+			velocidade -= x;
+		}
+	}
+	
+	void mostrar() {
+		System.out.println("Modelo: " + modelo + 
+			" Ano: " + ano + " Vel: " + velocidade + 
+			" Motor: " + motor);
+	}
+	
+	// método construtor
+	
+		public Carro (String mod, int an, float vel, boolean mt) {
+			modelo = mod;
+			ano = an;
+			velocidade = vel;
+			motor = mt;
+		}
+}
---
+++ teste/src/helloworld/Byeworld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Byeworld {
+
+	
+	public static void main(String[] args) {
+		String nome = "Rafaela";
+		
+		System.out.println(nome);
+	}
+	
+	
+}
