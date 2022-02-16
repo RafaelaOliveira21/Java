dd3dde8e605d7daba3dd527faeb6cd444e163361
Rafaela Oliveira <rafaela.oliveira0202@hotmail.com>
2022-02-15 23:27:14 -0300

    Primeira aula de Java

---
+++ Lista1/src/br/edu/fatecfranca/ex1/Produto.java
@@ -0,0 +1,41 @@
+package br.edu.fatecfranca.ex1;
+
+public class Produto {
+	
+	int id, qtde;
+	String descricao; 
+	float preco;
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(int i, int qt, String de, float pr) {
+		id = i;
+		qtde = qt;
+		descricao = de;
+		preco = pr;
+		
+	}
+	
+	public void comprar(int x) {
+		qtde += x;
+	}
+	
+	public void vender(int x) {
+		qtde -= x;
+	}
+	
+	public void subir(float x) {
+		preco += x;
+	}
+	
+	public void descer(float x) {
+		preco -= x;
+	}
+	
+	public void mostrar() {
+		System.out.println("Id: " + id + " Descrição:" + descricao + 
+				" Qtde " + qtde + " Preco " + preco);
+	}
+}
--- Lista1/src/br/edu/fatecfranca/exe0/Carro.java
+++ Lista1/src/br/edu/fatecfranca/exe0/Carro.java
@@ -1,46 +1,47 @@
 package br.edu.fatecfranca.exe0;
 
 public class Carro {
-	
+
 	// declaração das variáveis
 	// Java é fortemente tipado
-	
+
 	String modelo;
 	int ano;
 	float velocidade;
 	boolean motor; // motor true é ligado e false é desligado
-	
+
 	// criação dos métodos
-	void ligar () {
+	void ligar() {
 		motor = true;
 	}
-	void desligar () {
+
+	void desligar() {
 		motor = false;
 		velocidade = 0;
 	}
-	void acelerar(float x) { //x representa o quanto será acelerado
+
+	void acelerar(float x) { // x representa o quanto será acelerado
 		if (motor) {
 			velocidade += x;
 		}
 	}
-	void frear (float x) {
+
+	void frear(float x) {
 		if (motor) {
 			velocidade -= x;
 		}
 	}
-	
+
 	void mostrar() {
-		System.out.println("Modelo: " + modelo + 
-			" Ano: " + ano + " Vel: " + velocidade + 
-			" Motor: " + motor);
+		System.out.println("Modelo: " + modelo + " Ano: " + ano + " Vel: " + velocidade + " Motor: " + motor);
 	}
-	
+
 	// método construtor
-	
-		public Carro (String mod, int an, float vel, boolean mt) {
-			modelo = mod;
-			ano = an;
-			velocidade = vel;
-			motor = mt;
-		}
+
+	public Carro(String mod, int an, float vel, boolean mt) {
+		modelo = mod;
+		ano = an;
+		velocidade = vel;
+		motor = mt;
+	}
 }
